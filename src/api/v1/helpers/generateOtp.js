7f65fb55e998a13f6c3a0ec38a6d5c336d4d81db
Sameep Sehgal <sameepsam34@gmail.com>
2022-01-01 12:04:10 +0530

    Added function for random OTP generator

--- src/api/v1/helpers/generateOtp.js
+++ src/api/v1/helpers/generateOtp.js
@@ -1,2 +1,13 @@
+const otpGenerator = require('otp-generator');
 
-module.exports = () => {}
+export default () => {
+    return otpGenerator.generate(
+        6, 
+        { 
+            upperCaseAlphabets: false, 
+            specialChars: false ,
+            lowerCaseAlphabets: false,
+            digits: true
+        }
+    );
+}
